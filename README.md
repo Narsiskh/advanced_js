7b606580b8238a8476ccf7c80a0541b321ca6077
narsis <narsiskh.2013@gmail.com>
2024-04-20 15:17:03 +0330

    Learning closure in JS.

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ Scope when we have nested, functions Javascript variable lookup starts with the 
 <br>
 
 !! Remember that nested functions have access to variables declared in their own scope as well as variables declared in the outer scope.
+
+
+## Closure
+In JavaScript, when we return a function from another function, we are effectively returning a combination of the function definition along with the function's scope. This would let the function definition have an associated persistent memory which could hold on to live data between executions. That combination of the function and its scope chain is what is called a closure in JavaScript.
--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 /* Nested Function Scope */
-let a = 10
-function outer () {
-    let b = 20
-    function inner() {
-        let c = 30
-        console.log(a, b, c)
-    }
-    inner()
-}
-outer()
+// let a = 10
+// function outer () {
+//     let b = 20
+//     function inner() {
+//         let c = 30
+//         console.log(a, b, c)
+//     }
+//     inner()
+// }
+// outer() //10, 20, 30
 
+/* Closure */
+function outer() {
+    let counter = 0
+    function inner() {
+        counter++
+        console.log(counter)
+    }
+    /* inner() */
+    return inner
+}
+const fn = outer()
+fn()
+fn() //1 2
+/* outer() //1
+   outer() //1 1
+*/
